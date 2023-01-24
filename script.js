ad6272b5e762d027405b2f6a6594342876efbdf2
andrey-sd <andrey.sd.post@gmail.com>
2023-01-24 14:09:14 +0200

    ready

---
+++ script.js
@@ -0,0 +1,23 @@
+const button = document.getElementById('button');
+const scoreText = document.getElementById('scoreText');
+let score = 0;
+
+function hendler() {
+	score++;
+	console.log(score);
+	scoreText.innerHTML = score;
+}
+
+function newPosition() {
+	const x = Math.floor(Math.random() * 90);
+	const y = Math.floor(Math.random() * 90);
+	button.style.left = x + 'vw';
+	button.style.top = y + 'vh';
+}
+
+function endGame() {
+	alert('За 10 секунд ви натиснули ' + score + ' разів!')
+}
+
+setInterval(newPosition, 2000);
+setTimeout(endGame, 10000);
